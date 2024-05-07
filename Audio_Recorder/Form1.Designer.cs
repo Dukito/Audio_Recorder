b50088371dc54ed98346eae818850502b03471f6
PC <PC@DESKTOP-ONM0Q6F>
2024-05-07 21:51:33 +0300

    Add project files.

---
+++ Audio_Recorder/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Audio_Recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AudioRecorder = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AudioRecorder
+            // 
+            this.AudioRecorder.AutoSize = true;
+            this.AudioRecorder.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AudioRecorder.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.AudioRecorder.Location = new System.Drawing.Point(38, -2);
+            this.AudioRecorder.Name = "AudioRecorder";
+            this.AudioRecorder.Size = new System.Drawing.Size(289, 57);
+            this.AudioRecorder.TabIndex = 0;
+            this.AudioRecorder.Text = "Voice Recorder";
+            this.AudioRecorder.Click += new System.EventHandler(this.AudioRecorder_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(418, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(405, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(418, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Enter Name";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.CausesValidation = false;
+            this.button5.Image = global::Audio_Recorder.Properties.Resources.download__1_;
+            this.button5.Location = new System.Drawing.Point(244, 83);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 103);
+            this.button5.TabIndex = 7;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Image = global::Audio_Recorder.Properties.Resources.snip;
+            this.button4.Location = new System.Drawing.Point(128, 83);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 103);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Image = global::Audio_Recorder.Properties.Resources.snp;
+            this.button1.Location = new System.Drawing.Point(12, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 103);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(439, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(418, 83);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Resume Recording";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(420, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Time Recording:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(531, 202);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AudioRecorder);
+            this.Name = "Form1";
+            this.Text = "Voice Recorder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AudioRecorder;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Audio_Recorder/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+using System.Timers;
+
+namespace Audio_Recorder
+{
+    
+    public partial class Form1 : Form
+    {
+        string folder = " C:\\Users\\PC\\Desktop\\VOICE-RECORDS\\";
+        public void D()
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+        
+        int sec = 0;
+        int min = 0;
+        int hour = 0;
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+       
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int myfunc(string a, string b, int c, int d);
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = $"0{hour}:0{min}:0{sec}";
+            label2.Hide();
+            button4.Hide();
+            textBox1.Hide();
+            button3.Hide();
+            button2.Hide();
+            label1.Hide();
+            button5.Hide();
+            button6.Hide();
+            
+        }
+       
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Show();
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myfunc("open new Type waveaudio Alias recsound", "",0,0);
+            myfunc("record recsound","",0,0);
+            button1.Hide();
+            button4.Show();
+            button5.Hide();
+            label1.Show();
+            label2.Show();
+            sec = 0;
+            min = 0;
+            hour = 0;
+            timer1.Start();
+        }
+       
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string name = textBox1.Text;
+            D();
+
+            myfunc($"save recsound C:\\Users\\PC\\Desktop\\VOICE-RECORDS\\{name}.wav", "", 0, 0);
+            myfunc("close recsound", "", 0, 0);
+            button2.Hide();
+            button3.Hide();
+            textBox1.Hide();
+            button1.Show();
+            button5.Hide();
+            button6.Hide();
+
+
+
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            myfunc("pause recsound", "", 0, 0);
+            button5.Show();
+          
+            button4.Hide();
+            button1.Show();
+            timer1.Stop();
+           
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button4.Hide();
+            button1.Hide();
+            button3.Show();
+            button6.Show();
+            
+
+        }
+
+        private void AudioRecorder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            myfunc("open new Type waveaudio Alias recsound", "", 0, 0);
+            myfunc("record recsound", "", 0, 0);
+            button1.Hide();
+            button4.Show();
+            button5.Hide();
+            button6.Hide();
+            button3.Hide();
+            textBox1.Hide();
+            button2.Hide();
+            timer1.Start();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = $"0{hour}:0{min}:0{sec}";
+            sec++;
+            
+            if (sec > 9)
+            {
+                label1.Text = $"0{hour}:0{min}:{sec}";
+            }
+           if (sec == 59)
+            {
+                sec = 0;
+                min++;
+            }
+            if (min > 9)
+            {
+               
+                label1.Text = $"0{hour}:{min}:{sec}";
+            }
+            else if (min > 59)
+            {
+                min = 0;
+                hour++;
+            }
+            if (hour > 9)
+            {
+                label1.Text = $"{hour}:{min}:{sec}";
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+        
+    }
+
